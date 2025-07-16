dc6142012ea4cb24ccc1545d4e67a3c48baebe22
kento <na.ken0627@gmail.com>
2025-07-16 23:23:47 +0900

    feat:add put method

--- backend/Controllers/TodoItemsControllers.cs
+++ backend/Controllers/TodoItemsControllers.cs
@@ -35,13 +35,12 @@ public class TodoItemsController : ControllerBase
   // Todoのidを指定して、特定のTodoアイテムを取得するエンドポイント
   // POSTメソッドで新規作成した際に追加したアイテムデータをクライアントに返すために利用可能
   [HttpGet("{id}")]
-    public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
-    {
-        var todoItem = await _context.TodoItems.FindAsync(id);
-        if (todoItem == null) return NotFound();
-        return todoItem;
-    }
-
+  public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+  {
+    var todoItem = await _context.TodoItems.FindAsync(id);
+    if (todoItem == null) return NotFound();
+    return todoItem;
+  }
 
   // Todoアイテムを新規作成するエンドポイント
   // レスポンスを返す際にCreatedAtActionを使用して登録されたデータを返す
@@ -53,5 +52,25 @@ public class TodoItemsController : ControllerBase
     await _context.SaveChangesAsync();
     return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
   }
-    
+
+  //　Todoアイテムを更新する際にアイテムの存在を確認し、エラーハンドリングを分岐させるために必要
+  private bool TodoItemExists(int id) { return _context.TodoItems.Any(e => e.Id == id); }
+
+  [HttpPut("{id}")]
+  public async Task<IActionResult> PutTodoItem(int id, [FromBody] TodoItem todoItem)
+  {
+    if (id != todoItem.Id || !ModelState.IsValid) return BadRequest();
+    _context.Entry(todoItem).State = EntityState.Modified;
+    try
+    {
+      await _context.SaveChangesAsync();
+      return Ok(todoItem);
+    }
+    // DB更新時に競合発生をキャッチ。削除されて存在しないのか、同時に変更されて競合しているのかに分けてエラーハンドリング
+    catch (DbUpdateConcurrencyException)
+    {
+      if (!TodoItemExists(id)) return NotFound();
+      else throw;
+    }
+  }
 }
