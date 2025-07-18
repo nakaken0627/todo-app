40887e9edbc5d6b9a4c309c777d3b72f72818ec6
kento <na.ken0627@gmail.com>
2025-07-18 19:20:21 +0900

    feat:flexible fetching function of todos due to adopting some pattern

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 type Todo = {
+  id: string;
+  content: string;
+  dueDate: string;
+};
+type TodoForm = {
   content: string;
   dueDate: string;
 };
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [form, setForm] = useState<Todo>({ content: "", dueDate: "" });
+  const [form, setForm] = useState<TodoForm>({ content: "", dueDate: "" });
 
   const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.name === "dueDate" && e.target.value) {
@@ -25,7 +30,27 @@ function App() {
     }
   };
 
-  const addTodo = async (todo: Todo) => {
+  const fetchTodos = async (isComplete?: boolean) => {
+    const isCompleteParam =
+      isComplete !== undefined ? `?isComplete=${isComplete}` : "";
+
+    const response = await fetch(
+      `https://localhost:7027/api/todo-items${isCompleteParam}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("network error");
+    }
+    const data: Todo[] = await response.json();
+    setTodos(data);
+  };
+
+  const addTodo = async (todo: TodoForm) => {
     const response = await fetch("https://localhost:7027/api/todo-items", {
       method: "POST",
       headers: {
@@ -46,6 +71,10 @@ function App() {
     e.preventDefault();
     addTodo(form);
   };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
   return (
     <div>
@@ -74,6 +103,27 @@ function App() {
           <button type="submit">確定</button>
         </div>
       </form>
+      <div>
+        {todos.length === 0 ? (
+          <div>todoが登録されていません</div>
+        ) : (
+          todos.map((todo) => (
+            <div key={todo.id}>
+              <p>内容：{todo.content}</p>
+              <p>
+                締切：
+                {todo.dueDate
+                  ? new Date(todo.dueDate).toLocaleDateString()
+                  : "未設定"}
+              </p>
+            </div>
+          ))
+        )}
+      </div>
+      <div>
+        <button onClick={() => fetchTodos(true)}>完了</button>
+        <button onClick={() => fetchTodos(false)}>未完了</button>
+      </div>
     </div>
   );
 }
