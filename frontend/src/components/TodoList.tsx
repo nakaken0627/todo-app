8098d1301ba32acf596f2afd0625146dca93c011
kento <na.ken0627@gmail.com>
2025-07-26 20:33:36 +0900

    fix:remove unnecessary memo

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -35,59 +35,6 @@ export const TodoList = ({
             handleChangeStatus={handleChangeStatus}
             handleSetEditTodo={handleSetEditTodo}
           />
-          // <TodoItemPaper key={todo.id}>
-          //   <Grid
-          //     container
-          //     alignItems="center"
-          //     justifyItems="center"
-          //     spacing={1}
-          //   >
-          //     <Grid size={1}>
-          //       <Checkbox
-          //         onChange={() => handleChangeStatus(todo.id)}
-          //         checked={todo.isComplete}
-          //         color="primary"
-          //       />
-          //     </Grid>
-          //     <Grid size={8} sx={{ pl: 1 }}>
-          //       <Typography
-          //         variant="body1"
-          //         sx={{
-          //           textDecoration: todo.isComplete ? "line-through" : "none",
-          //           color: todo.isComplete ? "text.secondary" : "text.primary",
-          //         }}
-          //       >
-          //         {todo.content}
-          //       </Typography>
-          //       <Typography
-          //         variant="body2"
-          //         sx={{
-          //           color: "text.secondary",
-          //           mt: 0.5,
-          //         }}
-          //       >
-          //         期日：
-          //         {todo.dueDate
-          //           ? new Date(todo.dueDate).toLocaleDateString()
-          //           : "未設定"}{" "}
-          //       </Typography>
-          //     </Grid>
-          //     <Grid size={3} sx={{ textAlign: "right" }}>
-          //       <IconButton
-          //         onClick={() => handleSetEditTodo(todo.id)}
-          //         aria-label="edit"
-          //       >
-          //         <EditIcon />
-          //       </IconButton>
-          //       <IconButton
-          //         onClick={() => deleteTodo(todo.id)}
-          //         aria-label="delete"
-          //       >
-          //         <DeleteIcon />
-          //       </IconButton>
-          //     </Grid>
-          //   </Grid>
-          // </TodoItemPaper>
         ))
       )}
     </List>
